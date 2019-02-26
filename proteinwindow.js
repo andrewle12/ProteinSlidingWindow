9be3552ff1a72c14fb6457d2a9009a129b9f8615
Charos12 <andrew.le12@gmail.com>
2019-02-26 16:06:30 -0500

    html functionality

--- proteinwindow.js
+++ proteinwindow.js
@@ -1,31 +1,43 @@
 //Place full sequence here
-let sequence =
-  "MEHKEVVLLLLLFLKSGQGEPLDDYVNTQGASLFSVTKKQLGAGSIEECAAKCEEDEEFTCRAFQYHSKEQQCVIMAENRKSSIIIRMRDVVLFEKKVYLSECKTGNGKNYRGTMSKTKNGITCQKWSSTSPHRPRFSPATHPSEGLEENYCRNPDNDPQGPWCYTTDPEKRYDYCDILECEEECMHCSGENYDGKISKTMSGLECQAWDSQSPHAHGYIPSKFPNKNLKKNYCRNPDRELRPWCFTTDPNKRWELCDIPRCTTPPPSSGPTYQCLKGTGENYRGNVAVTVSGHTCQHWSAQTPHTHNRTPENFPCKNLDENYCRNPDGKRAPWCHTTNSQVRWEYCKIPSCDSSPVSTEQLAPTAPPELTPVVQDCYHGDGQSYRGTSSTTTTGKKCQSWSSMTPHRHQKTPENYPNAGLTMNYCRNPDADKGPWCFTTDPSVRWEYCNLKKCSGTEASVVAPPPVVLLPNVETPSEEDCMFGNGKGYRGKRATTVTGTPCQDWAAQEPHRHSIFTPETNPRAGLEKNYCRNPDGDVGGPWCYTTNPRKLYDYCDVPQCAAPSFDCGKPQVEPKKCPGRVVGGCVAHPHSWPWQVSLRTRFGMHFCGGTLISPEWVLTAAHCLEKSPRPSSYKVILGAHQEVNLEPHVQEIEVSRLFLEPTRKDIALLKLSSPAVITDKVIPACLPSPNYVVADRTECFITGWGETQGTFGAGLLKEAQLPVIENKVCNRYEFLNGRVQSTELCAGHLAGGTDSCQGDSGGPLVCFEKDKYILQGVTSWGLGCARPNKPGVYVRVSRFVTWIEGVMRNN";
+let sequence = "";
 
 //Number of amino acids per protein
-let numAmino = 30;
+let numAmino = 0;
 
-//Seperate sequence into individual letters
-let split = sequence.split("");
+var proteinWindow = (sequence, numAmino) => {
+  //Seperate sequence into individual letters
+  let split = sequence.split("");
 
-//Array of all built proteins
-let finalArray = [];
+  //Array of all built proteins
+  let finalArray = [];
 
-//Current protein being built
-let protein = "";
+  //Current protein being built
+  let protein = "";
 
-//To keep track of starting amino acid
-let startCounter = 0;
+  //To keep track of starting amino acid
+  let startCounter = 0;
 
-//While loop to build the proteins
-while (startCounter <= sequence.length) {
-  if (protein.length < numAmino) {
-    protein += split[startCounter];
-    startCounter++;
-  } else {
-    console.log(protein);
-    finalArray.push(protein);
-    protein = "";
-    startCounter = finalArray.length;
+  //While loop to build the proteins
+  while (startCounter <= sequence.length) {
+    if (protein.length < numAmino) {
+      protein += split[startCounter];
+      startCounter++;
+    } else {
+      printProtein(protein);
+      finalArray.push(protein);
+      protein = "";
+      startCounter = finalArray.length;
+    }
   }
-}
+};
+
+$("#submit").on("click", function() {
+  sequence = $("#sequence").val();
+  numAmino = $("#amino").val();
+  proteinWindow(sequence, numAmino);
+});
+
+var printProtein = peptide => {
+  $("#proteins").append(peptide);
+  $("#proteins").append("<br>");
+};
